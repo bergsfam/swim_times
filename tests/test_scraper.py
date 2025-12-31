8549d3a43b22a615aec3d0844018d3039bcf52d6
bergsfam <bergsfam@gmail.com>
2025-12-31 00:54:29 -0500

    Switch scraper to XML source

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -9,30 +9,53 @@ class ParseXmlPayloadTests(unittest.TestCase):
 
     def test_parse_xml_results_block(self) -> None:
         payload = (
-            "<html><xml>"
             "<results>"
-            "<event name=\"Boys 100 Backstroke\" date=\"February 17, 2024\" div=\"2\"></event>"
-            "<result rk=\"1\" nm=\"Krys Gorski\" gr=\"Sr\" sc=\"WAN\" ti=\"48.92\" mt=\"nedistrict24\" auto=\"yes\"></result>"
-            "<result rk=\"2\" nm=\"Jane Doe\" gr=\"Jr\" sc=\"SOM\" ti=\"50.10\" mt=\"nedistrict24\"></result>"
+            "<event name=\"Boys 200 Medley Relay\" date=\"February 17, 2024\" div=\"2\"></event>"
+            "<result rk=\"1\" nm=\"A\" gr=\"\" sc=\"HVUS\" sh=\"University School\" shx=\"nhs\" ct=\"Hunting Valley\" st=\"OH\" ll=\"41.485518, -81.427641\" ti=\"1:35.28\" mt=\"nedistrict24\" auto=\"yes\"/>"
+            "<result rk=\"2\" nm=\"A\" gr=\"\" sc=\"CIIH\" sh=\"Indian Hill\" ct=\"Cincinnati\" st=\"OH\" ll=\"39.186177, -84.347006\" ti=\"1:37.75\" mt=\"swdistrict24\" auto=\"yes\"/>"
             "</results>"
-            "</xml></html>"
         )
 
-        rows = self.scraper._parse_payload(payload.encode("utf-8"), "text/html")
+        rows = self.scraper._parse_payload(payload.encode("utf-8"), "application/xml")
 
         self.assertEqual(
             rows,
             [
-                {"rk": "1", "nm": "Krys Gorski", "gr": "Sr", "sc": "WAN", "ti": "48.92", "mt": "nedistrict24", "auto": "yes"},
-                {"rk": "2", "nm": "Jane Doe", "gr": "Jr", "sc": "SOM", "ti": "50.10", "mt": "nedistrict24", "auto": ""},
+                {
+                    "rk": "1",
+                    "nm": "A",
+                    "gr": "",
+                    "sc": "HVUS",
+                    "sh": "University School",
+                    "shx": "nhs",
+                    "ct": "Hunting Valley",
+                    "st": "OH",
+                    "ll": "41.485518, -81.427641",
+                    "ti": "1:35.28",
+                    "mt": "nedistrict24",
+                    "auto": "yes",
+                },
+                {
+                    "rk": "2",
+                    "nm": "A",
+                    "gr": "",
+                    "sc": "CIIH",
+                    "sh": "Indian Hill",
+                    "ct": "Cincinnati",
+                    "st": "OH",
+                    "ll": "39.186177, -84.347006",
+                    "ti": "1:37.75",
+                    "mt": "swdistrict24",
+                    "auto": "yes",
+                },
             ],
         )
 
     def test_parse_xml_missing_results_raises(self) -> None:
-        payload = "<html><xml><event name=\"Boys 100 Backstroke\"></event></xml></html>"
+        payload = "<results><event name=\"Boys 100 Backstroke\"></event></results>"
 
         with self.assertRaises(SwimMeetScraperError):
-            self.scraper._parse_payload(payload.encode("utf-8"), "text/html")
+            self.scraper._parse_payload(payload.encode("utf-8"), "application/xml")
 
 
 if __name__ == "__main__":  # pragma: no cover
